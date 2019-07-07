6efd9f911bfec38dc6bf3b3bb0a589bd9457fca3
Geon Lee <leegun2003@gmail.com>
2019-07-07 22:05:49 +0900

    ✨ keystore validation

--- src/index.js
+++ src/index.js
@@ -1,88 +1,92 @@
+import Caver from 'caver-js'
+
+const config = {
+  rpcURL: 'http://api.baobab.klaytn.net:8651'
+}
+
+const cav = new Caver(config.rpcURL)
 
 const App = {
+  auth: {
+    accessType: 'keystore',
+    keystore: '',
+    password: ''
+  },
+  start: async function() {},
 
-  start: async function () {
-
+  handleImport: async function() {
+    console.log(event)
+    const fileReader = new FileReader()
+    fileReader.readAsText(event.target.files[0])
+    fileReader.onload = event => {
+      console.log(event)
+      try {
+        if (!this.checkValidKeystore(event.target.result)) {
+          $('#message').text('유효하지 않은 keystore 파일입니다.')
+          return
+        }
+        this.auth.keystore = event.target.result
+        $('#message').text('keystore 통과, 비밀번호를 입력하세요.')
+        document.querySelector('#input-password').focus()
+      } catch (event) {
+        $('#message').text('유효하지 않은 keystore 파일입니다.')
+        return
+      }
+    }
   },
 
-  handleImport: async function () {
-
+  handlePassword: async function() {
+    this.auth.password = event.target.value
   },
 
-  handlePassword: async function () {
+  handleLogin: async function() {},
 
+  handleLogout: async function() {},
+
+  generateNumbers: async function() {},
+
+  submitAnswer: async function() {},
+
+  deposit: async function() {},
+
+  callOwner: async function() {},
+
+  callContractBalance: async function() {},
+
+  getWallet: function() {},
+
+  checkValidKeystore: function(keystore) {
+    const parsedKeystore = JSON.parse(keystore)
+    console.log(parsedKeystore)
+    const isValidKeystore =
+      parsedKeystore.version &&
+      parsedKeystore.id &&
+      parsedKeystore.address &&
+      parsedKeystore.crypto
+
+    return isValidKeystore
   },
 
-  handleLogin: async function () {
+  integrateWallet: function(privateKey) {},
 
-  },
+  reset: function() {},
 
-  handleLogout: async function () {
+  changeUI: async function(walletInstance) {},
 
-  },
+  removeWallet: function() {},
 
-  generateNumbers: async function () {
+  showTimer: function() {},
 
-  },
+  showSpinner: function() {},
 
-  submitAnswer: async function () {
+  receiveKlay: function() {}
+}
 
-  },
+window.App = App
 
-  deposit: async function () {
-
-  },
-
-  callOwner: async function () {
-
-  },
-
-  callContractBalance: async function () {
-
-  },
-
-  getWallet: function () {
-
-  },
-
-  checkValidKeystore: function (keystore) {
-
-  },
-
-  integrateWallet: function (privateKey) {
-
-  },
-
-  reset: function () {
-
-  },
-
-  changeUI: async function (walletInstance) {
-
-  },
-
-  removeWallet: function () {
-
-  },
-
-  showTimer: function () {
-
-  },
-
-  showSpinner: function () {
-
-  },
-
-  receiveKlay: function () {
-
-  }
-};
-
-window.App = App;
-
-window.addEventListener("load", function () {
-  App.start();
-});
+window.addEventListener('load', function() {
+  App.start()
+})
 
 var opts = {
   lines: 10, // The number of lines to draw
@@ -103,4 +107,4 @@ var opts = {
   left: '50%', // Left position relative to parent
   shadow: '0 0 1px transparent', // Box-shadow for the lines
   position: 'absolute' // Element positioning
-};
+}
